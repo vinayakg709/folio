89263a3d0f6aaa5679c01518dfead1923823f61a
unknown <vinayakg709@gmail.com>
2020-09-22 17:48:30 +0530

    connect section for web

--- Src/js/cursor.js
+++ Src/js/cursor.js
@@ -104,7 +104,7 @@ var cursor = {
         
         if (self.cursorEnlarged) {
             self.$dot.style.transform = 'translate(-50%, -50%) scale(0.75)';
-            self.$outline.style.transform = 'translate(-50%, -50%) scale(1.5)';
+            self.$outline.style.transform = 'translate(-50%, -50%) scale(2)';
         } else {
             self.$dot.style.transform = 'translate(-50%, -50%) scale(1)';
             self.$outline.style.transform = 'translate(-50%, -50%) scale(1)';
